176a136738b040e8fffb20499b4dabb93584e288
Aziza Alshahrani <105348844+M-Aziza@users.noreply.github.com>
2022-08-23 20:37:09 +0300

    Form validation

---
+++ form_validation/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Reduce from './components/Reduce';
+
+function App() {
+  return (
+    <div className="App">
+      <Reduce/>
+    </div>
+  );
+}
+
+export default App;
